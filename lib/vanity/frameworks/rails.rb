6ecfc6b35fb2f31a5c37c9e7f482ce068e777378
Fahim Ferdous <thefahim@gmail.com>
2012-01-11 13:36:12 -0500

    Only patch ActionController::TestCase if Rails.env.test?

--- lib/vanity/frameworks/rails.rb
+++ lib/vanity/frameworks/rails.rb
@@ -282,14 +282,16 @@ if defined?(ActionController)
     helper Vanity::Rails::Helpers
   end
 
-  module ActionController
-    class TestCase
-      alias :setup_controller_request_and_response_without_vanity :setup_controller_request_and_response
-      # Sets Vanity.context to the current controller, so you can do things like:
-      #   experiment(:simple).chooses(:green)
-      def setup_controller_request_and_response
-        setup_controller_request_and_response_without_vanity
-        Vanity.context = @controller
+  if Rails.env.test?
+    module ActionController
+      class TestCase
+        alias :setup_controller_request_and_response_without_vanity :setup_controller_request_and_response
+        # Sets Vanity.context to the current controller, so you can do things like:
+        #   experiment(:simple).chooses(:green)
+        def setup_controller_request_and_response
+          setup_controller_request_and_response_without_vanity
+          Vanity.context = @controller
+        end
       end
     end
   end
